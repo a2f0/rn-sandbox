eaa5928de908a2046b73e05e4a05c67e6b8d0bec
Dan Sullivan <dansullivan@gmail.com>
2025-11-27 11:48:46 -0500

    fix: builds

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  plugins: ['detox'],
   overrides: [
     {
       files: ['*.e2e.js'],
       env: {
-        'detox/detox': true,
         jest: true,
-        'jest/globals': true,
+      },
+      globals: {
+        device: 'readonly',
+        element: 'readonly',
+        by: 'readonly',
+        expect: 'readonly',
+        waitFor: 'readonly',
       },
     },
   ],
